652821e9c33e7f075a9a9855259d636d29bb103b
Saw Harry <sawharry2404@gmail.com>
2024-07-28 23:03:23 +0700

    add first xml and class of splash screen,with first images

---
+++ app/src/main/java/com/example/basicconcept/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.example.basicconcept;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+    }
+}
